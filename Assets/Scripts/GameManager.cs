b0c2841c229b00a46080507effac406a4b95bd29
Carlos Alejandro <alexco_99@hotmail.com>
2022-10-27 09:59:15 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    static public GameManager sharedInstance;
+    public GameStates currentGameState;
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Compara si esta en el juego y se pulsa el boton de pause
+        if (Input.GetButton("Pause") && currentGameState == GameStates.pause)
+        {
+
+        }
     }
+
+    // Quaternion.identity investigar que onda xd
+    // Instantiate(pelota, transform.position, Quaternion.idenity);
+
+    void SetNewGameState(GameStates newGameState)
+    {
+
+    }
+}
+
+public enum GameStates
+{
+    start,
+    inGame,
+    pause,
+    gameOver
 }
