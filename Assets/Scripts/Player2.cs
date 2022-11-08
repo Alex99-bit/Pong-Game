8cfb13045fa335cd07a925c66519760f6e840766
Carlos Alejandro <alexco_99@hotmail.com>
2022-11-08 09:24:14 -0600

    Campo y lógica para CPU añadida

---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    Transform posPelota;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Se busca el objeto ball y se extrae la posicion
+        posPelota = GameObject.FindWithTag("Ball").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector2(transform.position.x, posPelota.position.y);
+    }
+}
