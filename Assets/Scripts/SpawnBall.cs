db21cb5e33bc75d1f3aaeb1bcab3f5b7a749d14b
Carlos Alejandro <alexco_99@hotmail.com>
2022-11-13 23:42:07 -0600

    Update SpawnBall.cs

--- Assets/Scripts/SpawnBall.cs
+++ Assets/Scripts/SpawnBall.cs
@@ -6,6 +6,7 @@ public class SpawnBall : MonoBehaviour
 {
     [SerializeField] Transform ballTransform, spawn1, spawn2;
     Rigidbody2D ballRigid;
+    int gol;
 
     private void Awake()
     {
@@ -20,6 +21,30 @@ public class SpawnBall : MonoBehaviour
         ballRigid = this.GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameStates.inGame)
+        {
+            switch (gol)
+            {
+                case 1:
+                    gol = 0;
+                    // El jugador 1 recibio un gol, por ende se le añade una fuerza a la pelota en su direccion
+
+                    break;
+                case 2:
+                    gol = 0;
+                    // El jugador 2 recibio un gol, por ende se le añade una fuerza a la pelota en su direccion
+
+                    break;
+                default:
+                    // Si no pasa nada de eso, el gol se mantiene en 0
+                    gol = 0;
+                    break;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (GameManager.sharedInstance.currentGameState == GameStates.inGame)
@@ -29,7 +54,7 @@ public class SpawnBall : MonoBehaviour
             {
                 // Gol para player 2
                 GameManager.scorePlayer2++;
-
+                gol = 1;
                 // Saca el jugador 1, por ende se spawnea de su lado
                 ballTransform = spawn1;
             }
@@ -38,7 +63,7 @@ public class SpawnBall : MonoBehaviour
             {
                 // Gol para player 1
                 GameManager.scorePlayer1++;
-
+                gol = 2;
                 // Saca el jugador 2, por ende se spawnea de su lado
                 ballTransform = spawn2;
             }
